2f68ee7786b20cc595d6b614c290761b7759bfe3
fajruliq <mivtahviti@gmail.com>
2022-08-20 19:26:50 +0700

    Changes to be committed: 	modified:   application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ciunis/';
+$config['base_url'] = 'http://localhost/UAS_KelasC_2004030089/';
 
 /*
 |--------------------------------------------------------------------------
